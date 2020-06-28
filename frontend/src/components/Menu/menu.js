2cb3278aed1725c1ad343edbb6876f34eab2d4cf
Arhaam Patvi <thearhaam@gmail.com>
2020-06-28 19:07:41 +0530

    Implemented react-loader-spinner

--- frontend/src/components/Menu/menu.js
+++ frontend/src/components/Menu/menu.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import "./menu.css"
+import "./menu.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from 'react-loader-spinner';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
             cuisine: this.props.match.params.cuisine,
+            loading: true,
         }
     }
 
@@ -20,6 +23,8 @@ class Menu extends Component {
                     <h1>
                         {this.state.cuisine}
                     </h1>
+                    {this.state.loading ? <Loader type="Oval" color="rgb(79, 101, 121)" height={100} width={100} /> : null}
+
                 </div>
             </div>
         )
